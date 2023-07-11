679aa740ccafc420fdbb9dfc5fb959c0db7b5fc3
ChronoBean <106994923+ChronoBean@users.noreply.github.com>
2023-07-11 11:10:18 -0600

    Just a list of all the emotions of the trainingset

---
+++ emotions.py
@@ -0,0 +1,10 @@
+EMOTIONS = {
+    0: 'Angry',
+    1: 'Happy',
+    2: 'Sad',
+    3: 'Fear',
+    4: 'Disgust',
+    5: 'Suprise',
+    6: 'Neutral',
+    # Add more facial expressions as required
+}
